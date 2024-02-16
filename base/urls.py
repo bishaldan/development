f70855b1ea29eaf0ee18bff940677c686b80f7f4
bishaldan <124037829+bishaldan@users.noreply.github.com>
2024-02-16 18:12:51 +0545

    some updates

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('room/', views.room, name="room"),
-    path('login.html', views.login_page, name='login_page_html'),
-    path('main_student.html', views.main_student, name='main_student'),
+    path('login/', views.login_page, name='login_page'),
+    path('main_student/', views.main_student, name='main_student'),
 ]
